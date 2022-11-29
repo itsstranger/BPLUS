32150337bd7544b610ab642dc67a9c2505f35040
itsstranger <70693576+itsstranger@users.noreply.github.com>
2022-11-29 23:00:53 +0530

    2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
-    const effect = document.querySelector(".map");
-
+    const effect = document.querySelector(".super");
+    const efa = document.querySelector(".supera");
+    const efb = document.querySelector(".superb");
+const nav = document.querySelector(".nav");
 
 
 
@@ -8,15 +10,44 @@ window.onload = function() {
         window.addEventListener('scroll',scrollEffect);
 
     function scrollEffect(){
-        if(window.scrollY>=500){
+        if(window.scrollY>=300){
             effect.style.opacity = '1';
             effect.style.transform = 'translateX(0px)';
             effect.style.transition = '1s ease-in-out';
+
+
+
+          nav.style.transform = 'scale(100%)';
+          nav.style.transition = '0.3s linear';
+
         }else{
             effect.style.opacity = '0';
             effect.style.transform = 'translateX(-50px)';
+          nav.style.transform = 'scale(100%)';
            
         }
+      
+      if(window.scrollY>=750){
+        efa.style.opacity = '1';
+        efa.style.transform = 'translateX(0px)';
+        efa.style.transition = '1s ease-in-out';
+
+      }else{
+        efa.style.opacity = '0';
+            efa.style.transform = 'translateX(-50px)';
+      }
+
+      if(window.scrollY>=1250){
+       
+        efb.style.opacity = '1';
+        efb.style.transform = 'translateX(0px)';
+        efb.style.transition = '1s ease-in-out';
+
+      }else{
+        efb.style.opacity = '0';
+            efb.style.transform = 'translateX(-50px)';
+      }
     }
     scrollEffect()
 }
+
